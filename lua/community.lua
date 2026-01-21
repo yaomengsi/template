94996f63b02c7b061c872e81be6d1e69157e8680
miles <yaomengsi@outlook.com>
2026-01-22 06:52:54 +0800

    feat(opencode): add opencode, and remove avante

--- lua/community.lua
+++ lua/community.lua
@@ -14,7 +14,7 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.marks-nvim" },
   -- ai completion
-  { import = "astrocommunity.completion.avante-nvim" },
+  -- { import = "astrocommunity.completion.avante-nvim" },
   -- language
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.rust" },
